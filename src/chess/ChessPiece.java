4551b00b6e5c474df2050ce0647f8f7be92d2598
Felipe Affeldt <felipeaffeldt@gmail.com>
2023-03-17 13:58:45 -0300

    Possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,7 +8,7 @@ import boardgame.Piece;
  *
  * @author Felipe
  */
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
     
     private Color color;
 
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -8,16 +8,22 @@ import chess.Color;
  *
  * @author Felipe
  */
-public class King extends ChessPiece
-{
+public class King extends ChessPiece {
 
     public King(Board board, Color color) {
         super(board, color);
     }
-    
-   @Override
+
+    @Override
     public String toString() {
         return "K";
     }
-    
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+
+    }
+
 }
